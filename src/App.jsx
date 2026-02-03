57d8d2c786ffe773a5c205a0ba989cb750e4273d
Marijo Kis <marijo.kis@q.agency>
2026-02-03 20:52:29 +0100

    chore: init

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import EffectDep from "./components/EffectDep";
+import EffectEmptyDep from "./components/EffectEmptyDep";
+import EffectNoDep from "./components/EffectNoDep";
+import users from "./mocks/users";
+import Username from "./components/Username";
+
+function App() {
+  const [test, setTest] = useState(0);
+
+  const nums = [
+    {
+      unique: 1,
+      value: 1,
+    },
+    {
+      unique: 2,
+      value: 2,
+    },
+    {
+      unique: 3,
+      value: 3,
+    },
+    {
+      unique: 4,
+      value: 5,
+    },
+    {
+      unique: 5,
+      value: 5,
+    },
+  ];
+
+  return (
+    <>
+      <EffectNoDep />
+      <EffectEmptyDep />
+      <EffectDep test={test} />
+      <button onClick={() => setTest(test + 1)}>Change test</button>
+
+      {users.map((user) => (
+        <Username
+          firstname={user.firstname}
+          lastname={user.lastname}
+          key={user.id}
+        />
+      ))}
+
+      <ul></ul>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/EffectEmptyDep/EffectEmptyDep.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function EffectEmptyDep() {
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  useEffect(() => {
+    console.log("Pozvan je useEffect u EffectEmptyDep!");
+  }, []);
+
+  return (
+    <>
+      <p>useEffect empty dep</p>
+      <div>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>Click</button>
+      </div>
+      <div>
+        <span>{flag ? "Yes" : "No"}</span>
+        <button onClick={() => setFlag(!flag)}>Click</button>
+      </div>
+    </>
+  );
+}
+
+export default EffectEmptyDep;
